b6f50fc3b69b4f4a8778f0f885e2cfe69bc1b2ff
Eder Diego de Sousa <me@diego.dev.br>
2022-05-18 23:01:43 -0300

    User, Role

---
+++ backend/src/main/java/br/dev/diego/dslearn/entities/Role.java
@@ -0,0 +1,69 @@
+package br.dev.diego.dslearn.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Objects;
+import java.util.StringJoiner;
+
+@Entity
+@Table(name = "tb_role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String authority;
+
+    public Role() {
+    }
+
+    public Role(Long id) {
+        this.id = id;
+    }
+
+    public Role(Long id, String authority) {
+        this.id = id;
+        this.authority = authority;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Role)) return false;
+        Role role = (Role) o;
+        return Objects.equals(id, role.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String
+    toString() {
+        return new StringJoiner(", ", Role.class.getSimpleName() + "[", "]")
+                .add("id=" + id)
+                .add("authority='" + authority + "'")
+                .toString();
+    }
+}
