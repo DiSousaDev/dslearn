5d55521d35387b083925752b662abe7f21a2f701
Eder Diego de Sousa <me@diego.dev.br>
2022-05-21 16:30:08 -0300

    Course

---
+++ backend/src/main/java/br/dev/diego/dslearn/entities/Notification.java
@@ -0,0 +1,98 @@
+package br.dev.diego.dslearn.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.Instant;
+import java.util.Objects;
+import java.util.StringJoiner;
+
+@Entity
+@Table(name = "tb_notification")
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String text;
+    private Instant moment;
+    private boolean read = false;
+    private String route;
+
+    public Notification() {
+    }
+
+    public Notification(Long id, String text, Instant moment, boolean read, String route) {
+        this.id = id;
+        this.text = text;
+        this.moment = moment;
+        this.read = read;
+        this.route = route;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public boolean isRead() {
+        return read;
+    }
+
+    public void setRead(boolean read) {
+        this.read = read;
+    }
+
+    public String getRoute() {
+        return route;
+    }
+
+    public void setRoute(String route) {
+        this.route = route;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Notification)) return false;
+        Notification that = (Notification) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Notification.class.getSimpleName() + "[", "]")
+                .add("id=" + id)
+                .add("text='" + text + "'")
+                .add("moment=" + moment)
+                .add("read=" + read)
+                .add("route='" + route + "'")
+                .toString();
+    }
+}
